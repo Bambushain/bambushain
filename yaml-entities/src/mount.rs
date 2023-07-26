503d1a34203301bf719136be5175e693973c43d5
Imanuel Ulbricht <me@imanuel.dev>
2023-07-26 20:40:04 +0200

    Remove yaml database

--- yaml-entities/src/mount.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Ord, PartialOrd, Eq, PartialEq, Clone)]
-pub struct Mount {
-    pub name: String,
-}
--- yaml-entities/src/savage_mount.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Ord, PartialOrd, Eq, PartialEq, Clone)]
-pub struct SavageMount {
-    pub name: String,
-}
