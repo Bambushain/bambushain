6a0ba7f9338c5b969d366fa39609874bb4343fa0
bambushain <me@imanuel.dev>
2024-01-19 21:36:50 +0000

    TG-224 Confirm zum Verlassen vom Hain
    Added frontend to leave a grove

--- frontend/sections/bamboo/src/lib.rs
+++ frontend/sections/bamboo/src/lib.rs
@@ -1,6 +1,5 @@
-pub use api::get_users;
 pub use pages::calendar::*;
 pub use pages::user::*;
 
-mod api;
+pub mod api;
 pub mod pages;
--- frontend/sections/layout/src/api/mod.rs
+++ frontend/sections/layout/src/api/mod.rs
@@ -1,4 +1,6 @@
 pub use authentication::*;
+pub use bamboo_frontend_section_authentication::api::get_my_profile;
+pub use bamboo_frontend_section_bamboo::api::get_users;
 pub use grove::*;
 pub use my::*;
 
