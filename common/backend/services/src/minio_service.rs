2c243c25a00ebb3654566b4ccb44719e4a48ea51
Imanuel Ulbricht <me@imanuel.dev>
2024-02-09 01:22:44 +0000

    CHORE: Small cleanup

--- common/backend/services/src/minio_service.rs
+++ common/backend/services/src/minio_service.rs
@@ -32,7 +32,7 @@ impl MinioClient {
             None,
             None,
         )
-        .map_err(|err| S3Error::Credentials(err))?;
+        .map_err(S3Error::Credentials)?;
         let mut bucket = Bucket::new(bucket_name.as_str(), region, credentials)?.with_path_style();
         if use_path_style {
             bucket.set_path_style();
